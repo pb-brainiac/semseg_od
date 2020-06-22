f857b915629941399199dbfa9853c956b5b117a3
Petra Bevandic <petra.bevandic@gmail.com>
2020-06-22 16:17:47 +0200

    Move evaluation to separate module

---
+++ evaluation.py
@@ -0,0 +1,72 @@
+import torch
+import numpy as np
+
+def get_img_conf_mat(pred, true, size):
+    eye_matrix = torch.eye(size[0]).cuda()
+    pred_OH = eye_matrix[pred].to(torch.float64)
+    true_OH = eye_matrix[true].to(torch.float64)
+    return torch.einsum("ni,nj->ij", true_OH, pred_OH).to(torch.int64)
+
+def compute_errors(conf_mat, name, class_info, verbose=False, nc=None):
+    num_classes = nc if nc is not None else model.num_classes
+    num_correct = conf_mat.trace()
+    print(num_classes)
+    total_size = conf_mat.sum()
+    avg_pixel_acc = num_correct / total_size * 100.0
+    TPFP = conf_mat.sum(0)
+    TPFN = conf_mat.sum(1)
+    FN = TPFN - conf_mat.diagonal()
+    FP = TPFP - conf_mat.diagonal()
+    class_iou = np.zeros(num_classes)
+    class_recall = np.zeros(num_classes)
+    class_precision = np.zeros(num_classes)
+    print(name, 'errors:')
+    for i in range(num_classes):
+        TP = conf_mat[i, i]
+        if (TP + FP[i] + FN[i]) > 0:
+            class_iou[i] = (TP / (TP + FP[i] + FN[i])) * 100.0
+        else:
+            class_iou[i] = 0
+        if TPFN[i] > 0:
+            class_recall[i] = (TP / TPFN[i]) * 100.0
+        else:
+            class_recall[i] = 0
+        if TPFP[i] > 0:
+            class_precision[i] = (TP / TPFP[i]) * 100.0
+        else:
+            class_precision[i] = 0
+
+        class_name = class_info[i][3]
+        if verbose:
+            print('\t%d IoU accuracy = %.2f %%' % (i, class_iou[i]))
+            print('\t%d recall = %.2f %%' % (i, class_recall[i]))
+            print('\t%d precision = %.2f %%' % (i, class_precision[i]))
+    avg_class_iou = class_iou.mean()
+    avg_class_recall = class_recall.mean()
+    avg_class_precision = class_precision.mean()
+    print('IoU mean class accuracy -> TP / (TP+FN+FP) = %.2f %%' %
+          avg_class_iou)
+    print('mean class recall -> TP / (TP+FN) = %.2f %%' % avg_class_recall)
+    print('mean class precision -> TP / (TP+FP) = %.2f %%' %
+          avg_class_precision)
+    print('pixel accuracy = %.2f %%' % avg_pixel_acc)
+
+def segment_image(model, batch, args, conf_mats, ood_id=-1, nc=None):
+    num_classes = nc if nc is not None else model.num_classes
+    name = batch['name'][0]
+    img = torch.autograd.Variable(batch['image'].cuda(
+            non_blocking=True), requires_grad=True)
+
+    with torch.no_grad():
+        probs, conf_probs = model(img, batch['image'].shape[2:])
+        pred = probs.max(dim=1)[1][0]
+        pred_w_outlier = pred.clone()
+        pred_w_outlier[conf_probs[0] < 0.5] = ood_id
+
+        true = None
+        if batch['labels'] is not None:
+            labels = batch['labels'][0].cuda()
+            conf_mats['seg'] += get_img_conf_mat(pred[labels<num_classes], labels[labels<num_classes], conf_mats['seg'].shape)
+            conf_mats['seg_w_outlier'] += get_img_conf_mat(pred_w_outlier[labels<num_classes], labels[labels<num_classes], conf_mats['seg_w_outlier'].shape)
+
+    return pred, pred_w_outlier, conf_probs
--- inference.py
+++ inference.py
@@ -13,11 +13,16 @@ import numpy as np
 import PIL.Image as pimg
 import matplotlib.pyplot as plt
 import importlib.util
+import readers.cityscapes_reader as city_reader
 import readers.wilddash_reader as wd_reader
 import readers.lsun_reader as lsun_reader
 import readers.pascal_wd_reader as pascal_wd_reader
+import readers.viper_reader as viper_reader
 import sklearn.metrics as sm
+import evaluation
 import pdb
+
+
 
 class Logger(object):
 
@@ -33,61 +38,11 @@ class Logger(object):
         for f in self.files:
             f.flush()
 
-
 def import_module(name, path):
     spec = importlib.util.spec_from_file_location(name, path)
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
     return module
-
-def get_img_conf_mat(pred, true, size):
-    eye_matrix = torch.eye(size[0]).cuda()
-    pred_OH = eye_matrix[pred].to(torch.float64)
-    true_OH = eye_matrix[true].to(torch.float64)
-    return torch.einsum("ni,nj->ij", true_OH, pred_OH).to(torch.int64)
-
-def compute_errors(conf_mat, name, class_infoi, verbose=False):
-    num_correct = conf_mat.trace()
-    print(num_classes)
-    total_size = conf_mat.sum()
-    avg_pixel_acc = num_correct / total_size * 100.0
-    TPFP = conf_mat.sum(0)
-    TPFN = conf_mat.sum(1)
-    FN = TPFN - conf_mat.diagonal()
-    FP = TPFP - conf_mat.diagonal()
-    class_iou = np.zeros(num_classes)
-    class_recall = np.zeros(num_classes)
-    class_precision = np.zeros(num_classes)
-    print(name, 'errors:')
-    for i in range(num_classes):
-        TP = conf_mat[i, i]
-        if (TP + FP[i] + FN[i]) > 0:
-            class_iou[i] = (TP / (TP + FP[i] + FN[i])) * 100.0
-        else:
-            class_iou[i] = 0
-        if TPFN[i] > 0:
-            class_recall[i] = (TP / TPFN[i]) * 100.0
-        else:
-            class_recall[i] = 0
-        if TPFP[i] > 0:
-            class_precision[i] = (TP / TPFP[i]) * 100.0
-        else:
-            class_precision[i] = 0
-
-        class_name = class_info[i][3]
-        if verbose:
-            print('\t%d IoU accuracy = %.2f %%' % (i, class_iou[i]))
-            print('\t%d recall = %.2f %%' % (i, class_recall[i]))
-            print('\t%d precision = %.2f %%' % (i, class_precision[i]))
-    avg_class_iou = class_iou.mean()
-    avg_class_recall = class_recall.mean()
-    avg_class_precision = class_precision.mean()
-    print('IoU mean class accuracy -> TP / (TP+FN+FP) = %.2f %%' %
-          avg_class_iou)
-    print('mean class recall -> TP / (TP+FN) = %.2f %%' % avg_class_recall)
-    print('mean class precision -> TP / (TP+FP) = %.2f %%' %
-          avg_class_precision)
-    print('pixel accuracy = %.2f %%' % avg_pixel_acc)
 
 def colorize_labels(y, class_colors):
     width = y.shape[1]
@@ -143,32 +98,15 @@ def store_conf(img_raw, conf, name, conf_type='logit'):
     img = pimg.fromarray(img)
     saver_pool.apply_async(img.save, [save_path])
 
-def segment_image(model, batch, args, conf_mats):
-    name = batch['name'][0]
-    img = torch.autograd.Variable(batch['image'].cuda(
-            non_blocking=True), requires_grad=True)
-
-    with torch.no_grad():
-        probs, conf_probs = model(img, batch['image'].shape[2:])
-        pred = probs.max(dim=1)[1][0]
-        pred_w_outlier = pred.clone()
-        pred_w_outlier[conf_probs[0] < 0.5] = ood_id
-
-        true = None
-        if batch['labels'] is not None:
-            labels = batch['labels'][0].cuda()
-            conf_mats['seg'] += get_img_conf_mat(pred[labels<num_classes], labels[labels<num_classes], conf_mats['seg'].shape)
-            conf_mats['seg_w_outlier'] += get_img_conf_mat(pred_w_outlier[labels<num_classes], labels[labels<num_classes], conf_mats['seg_w_outlier'].shape)
-
-    if args.save_outputs:
-        pred = pred.detach().cpu().numpy().astype(np.int32)
-        pred_w_outlier = pred_w_outlier.detach().cpu().numpy().astype(np.int32)
-        conf_probs = conf_probs.detach().cpu().numpy()
-        img_raw = wd_dataset.denormalize(batch['image'][0],
-                                      batch['mean'][0].numpy(), batch['std'][0].numpy())
-        store_images(img_raw, pred, true, class_info, 'segmentation/'+name)
-        store_images(img_raw, pred_w_outlier, true, class_info, 'seg_with_conf/'+ name)
-        store_conf(img_raw, conf_probs, name, 'probs')
+def store_outputs(batch, pred, pred_w_outlier, conf_probs):
+    pred = pred.detach().cpu().numpy().astype(np.int32)
+    pred_w_outlier = pred_w_outlier.detach().cpu().numpy().astype(np.int32)
+    conf_probs = conf_probs.detach().cpu().numpy()
+    img_raw = wd_dataset.denormalize(batch['image'][0],
+                                  batch['mean'][0].numpy(), batch['std'][0].numpy())
+    store_images(img_raw, pred, true, class_info, 'segmentation/'+name)
+    store_images(img_raw, pred_w_outlier, true, class_info, 'seg_with_conf/'+ name)
+    store_conf(img_raw, conf_probs, name, 'probs')
 
 def evaluate_segmentation():
     conf_mats = {}
@@ -178,7 +116,10 @@ def evaluate_segmentation():
     log_interval = max(len(wd_data_loader) // 5, 1)
     for step, batch in enumerate(wd_data_loader):
         try:
-            segment_image(model, batch, args, conf_mats)
+            pred, pred_w_outlier, conf_probs = evaluation.segment_image(model, batch, args, conf_mats, ood_id, num_classes)
+            if args.save_outputs:
+                store_outputs(batch, pred, pred_w_outlier, conf_probs)
+
         except Exception as e:
             print('failed on image: {}'.format(batch['name'][0]))
             print('error: {}'.format(e))
@@ -189,10 +130,10 @@ def evaluate_segmentation():
 
     print('\nSegmentation:')
     conf_mats['seg'] = conf_mats['seg'].cpu().numpy()
-    compute_errors(conf_mats['seg'], 'Validation', class_info)
+    evaluation.compute_errors(conf_mats['seg'], 'Validation', class_info, nc=num_classes)
     print('\nSegmentation with confidence:')
     conf_mats['seg_w_outlier'] = conf_mats['seg_w_outlier'].cpu().numpy()
-    compute_errors(conf_mats['seg_w_outlier'], 'Validation', class_info)
+    evaluation.compute_errors(conf_mats['seg_w_outlier'], 'Validation', class_info, nc=num_classes)
 
 def evaluate_AP_negative():
     gt_wd = torch.ByteTensor([])
@@ -282,7 +223,7 @@ def get_args():
     parser.add_argument('--verbose', type=int, default=1)
     parser.add_argument('--AP-iters', type=int, default=50)
     parser.add_argument('--save-name', type=str, default='')
-    parser.add_argument('--data_path', type=str, default='./data/')
+    parser.add_argument('--data-path', type=str, default='./data/')
     return parser.parse_args()
 
 def prepare_for_saving():
@@ -323,18 +264,18 @@ ood_id = wd_reader.DatasetReader.ood_id
 num_classes = wd_reader.DatasetReader.num_classes
 
 
-wd_dataset = wd_reader.DatasetReader(args)
+wd_dataset = wd_reader.DatasetReader(args)#, subset='val')
 wd_data_loader = DataLoader(wd_dataset, batch_size=1,
-                         num_workers=1, pin_memory=True, shuffle=False)
-lsun_dataset = lsun_reader.DatasetReader(args)
-lsun_data_loader = DataLoader(lsun_dataset, batch_size=1,
-                         num_workers=1, pin_memory=True, shuffle=True)
-pascal_wd_dataset = pascal_wd_reader.DatasetReader(args)
-pascal_wd_data_loader = DataLoader(pascal_wd_dataset, batch_size=1,
-                         num_workers=0, pin_memory=True, shuffle=True)
+                         num_workers=8, pin_memory=True, shuffle=False)
+#lsun_dataset = lsun_reader.DatasetReader(args)
+#lsun_data_loader = DataLoader(lsun_dataset, batch_size=1,
+#                         num_workers=1, pin_memory=True, shuffle=True)
+#pascal_wd_dataset = pascal_wd_reader.DatasetReader(args)
+#pascal_wd_data_loader = DataLoader(pascal_wd_dataset, batch_size=1,
+#                         num_workers=0, pin_memory=True, shuffle=True)
 
 
 
 evaluate_segmentation()
-evaluate_AP_negative()
-evaluate_AP_patches()
+#evaluate_AP_negative()
+#evaluate_AP_patches()
